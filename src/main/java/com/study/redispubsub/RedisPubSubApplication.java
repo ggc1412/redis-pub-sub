bf8beee913b3905191c6753bc34df500180474a8
maro <maro@kimmaro-MacBookPro.local>
2023-07-19 18:51:42 +0900

    ✨ redis hello-world

--- src/main/java/com/study/redispubsub/RedisPubSubApplication.java
+++ src/main/java/com/study/redispubsub/RedisPubSubApplication.java
@@ -2,12 +2,20 @@ package com.study.redispubsub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
 public class RedisPubSubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RedisPubSubApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(RedisPubSubApplication.class, args);
+		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+		String val = template.opsForValue().get("key1");
+		System.out.println(val);
+
+		val = template.opsForValue().get("key2");
+		System.out.println(val);
 	}
 
 }
