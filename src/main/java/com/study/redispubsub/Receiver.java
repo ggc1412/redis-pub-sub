c2d0a2987ed04bd59787c2b8437ab754a5019257
maro <maro@kimmaro-MacBookPro.local>
2023-07-20 11:49:00 +0900

    ✨ Pub/Sub 스터디 코드 추가

---
+++ src/main/java/com/study/redispubsub/Receiver.java
@@ -0,0 +1,19 @@
+package com.study.redispubsub;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Receiver {
+    private AtomicInteger counter = new AtomicInteger();
+
+    public void receiveMessage(String message) {
+        log.info("Received <" + message + ">");
+        int newCount = counter.incrementAndGet();
+        log.info("Updated Count : " + newCount);
+    }
+
+    public int getCount() {
+        return counter.get();
+    }
+}
--- src/main/java/com/study/redispubsub/RedisPubSubApplication.java
+++ src/main/java/com/study/redispubsub/RedisPubSubApplication.java
@@ -1,21 +1,52 @@
 package com.study.redispubsub;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
+@Slf4j
 @SpringBootApplication
 public class RedisPubSubApplication {
 
-	public static void main(String[] args) {
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+											MessageListenerAdapter listenerAdapter) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	Receiver receiver() {
+		return new Receiver();
+	}
+
+	@Bean
+	StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
 		ApplicationContext ctx = SpringApplication.run(RedisPubSubApplication.class, args);
 		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
-		String val = template.opsForValue().get("key1");
-		System.out.println(val);
 
-		val = template.opsForValue().get("key2");
-		System.out.println(val);
+		log.info("Sending message......");
+		Thread.sleep(1000);
+		template.convertAndSend("chat", "Hello from Redis!");
 	}
 
 }
